30af38abe193d2c58d851ddfb3e7f4cc2140b2a2
seyoungjoy <diddpwl80@gmail.com>
2021-10-10 17:56:08 +0900

    1-5

--- Kimtaewon-Algorithm/섹션1/1-6.js
+++ Kimtaewon-Algorithm/섹션1/1-6.js
@@ -1,11 +1,13 @@
 
+
 function solution(arr){
-    let answer, min=Number.MAX_SAFE_INTEGER;
+    let answer, nmi=Number.MAX_SAFE_INTEGER; //min을 최대한 큰 수로 먼저 잡아줘야 아래에서 다른 값들과 비교할 때 편리하기 때문.
     for(let i=0; i<arr.length; i++){
         if(arr[i]<min) min=arr[i];
     }
     answer=min;
     return answer;
+
 }
 
 let arr=[5, 7, 1, 3, 2, 9, 11];
