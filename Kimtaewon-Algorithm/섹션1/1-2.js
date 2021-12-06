6c3a22dc7aa925cdbb9c0ef80f494f8ed8e6e30e
seyoungjoy <diddpwl80@gmail.com>
2021-12-07 08:52:35 +0900

    1-2

---
+++ Kimtaewon-Algorithm/섹션1/1-2.js
@@ -0,0 +1,26 @@
+//나의 풀이
+// function solution(a, b, c){
+//     if(a>b) max=a;
+//     else max=b;
+//     if(max>c) max=max;
+    
+//     if(a+b+c-max > max) console.log("yes");
+//     else console.log("no");
+    
+//  }
+
+//  console.log(solution(6, 7, 11));
+
+ //강의 참고 풀이
+ function solution(a,b,c){
+     let answer="YES";
+     let max;
+     let sum=a+b+c;
+
+     if(a>b) max=a;
+     else max=b;
+     if(c>max) max=c;
+     if((sum-max)>=max) answer="NO";
+     return answer;
+ }
+ console.log(solution(6,7,11));
--- README.md
+++ README.md
@@ -17,6 +17,10 @@ a>b ? answer=a : answer=b;
 
 ---
 2. [삼각형 판별하기](Kimtaewon-Algorithm/섹션1/1-2.js)<br>
+**삼각형 만드는 공식**<br>
+- 가장 긴 막대의 길이가 나머지 두 막대의 합보다 크거나 같아야 삼각형이 만들어진다.<br>
+이 공식을 이용해 a,b,c 중 최대값 max 를 구해서 해결할 수 있다.
+---
 3. [연필개수](Kimtaewon-Algorithm/섹션1/1-3.js)<br>
 
 *사용된 함수<br>
