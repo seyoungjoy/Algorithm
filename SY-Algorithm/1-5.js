94363ff5e8b3590f049968aec44f0c0d0efc40d4
seyoungjoy <diddpwl80@gmail.com>
2021-12-11 20:16:27 +0900

    1-5

--- SY-Algorithm/1-5.js
+++ SY-Algorithm/1-5.js
@@ -1,40 +1,39 @@
 //최솟값 구하기
 
-// function solution(arr){
-//     let answer;
-//     let min=Number.MAX_SAFE_INTEGER;
+function solution(arr){
+    let answer;
+    let min=Number.MAX_SAFE_INTEGER;
 
-//     for(let i=0; i<arr.length; i++){
-//         if(arr[i]<min) min=arr[i];
-//     }
-//     answer=min;
-//     return answer;
-// }
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]<min) min=arr[i];
+    }
+    answer=min;
+    return answer;
+}
 
-// let arr=[1,2,3,4,5];
-// alert(solution(arr));
+let arr=[1,2,3,4,5];
+alert(solution(arr));
 
 //[응용:최댓값 구해보기]
-// let max=Number.MIN_SAFE_INTEGER;
+let max=Number.MIN_SAFE_INTEGER;
 
-// function solutionMax(arr){
-//     let answer;
-//     for(let i =0; i<arr.length; i++){
-//         if(arr[i]>max) max=arr[i];
-//     }
-//     answer = max;
-//     return answer;
-// }
+function solutionMax(arr){
+    let answer;
+    for(let i =0; i<arr.length; i++){
+        if(arr[i]>max) max=arr[i];
+    }
+    answer = max;
+    return answer;
+}
 
-// let arr = [1,3,5];
-// console.log(solutionMax(arr));
+let arr = [1,3,5];
+console.log(solutionMax(arr));
 
 //[응용] : filter 메소드로 구해보기
 
-function findMinValue(value){
-    const numbers = [1,2,3,4,5];
-    const minNumb = numbers.filter(number => number < ))
-    
-}
+//고민중 ...
 
-// numbers.filter(numbers => numbers(value) <numbersc.length)
+// let arr = [1,2,3,4,5];
+// let result = arr.filter(function(){
+    
+// })
