c1db3ec6a5886706952cbde182fffbd603228807
seyoungjoy <diddpwl80@gmail.com>
2022-01-05 10:23:32 +0900

    1-8

---
+++ SY-Algorithm/1-8.js
@@ -0,0 +1,16 @@
+//일곱 난쟁이
+//7 -> 9
+// 7명의 키의 합이 100
+// 각각의 키는 100이 넘지 않음
+// 키는 모두 다름
+// 일곱 난쟁이를 찾는 프로그램 작성
+// 가능한 정답이 여러가지일때는 아무거나 출력
+
+function solution(arr){
+    let answer=arr;
+
+    return answer;
+}
+
+let arr=[20, 7, 23, 19, 10, 15, 25, 8, 13];
+console.log(solution(arr));
