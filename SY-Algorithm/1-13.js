778c683a7c03dcca768b6aaa7d0e765d20be53f8
seyoungjoy <diddpwl80@gmail.com>
2022-02-20 11:00:54 +0900

    1-13

--- SY-Algorithm/1-13.js
+++ SY-Algorithm/1-13.js
@@ -1,3 +1,17 @@
 //대문자 변환
 //대문자는 소문자로
 //대문자는 소문자로
+//문자열의 길이는 100을 넘지 않는다.
+
+function solution(str){
+    answer = ''
+    str.split('').forEach(item =>{
+        if(item === item.toUpperCase()){
+            answer += item.toLowerCase();
+        } else{
+            answer += item.toUpperCase();
+        }
+    })
+    return console.log(answer)
+}
+solution('StuDY')
