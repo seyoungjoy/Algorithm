383e6e990ea94ebdeec85d102d38a95afdbe1e62
seyoungjoy <diddpwl80@gmail.com>
2022-02-23 23:42:11 +0900

    1-16

--- SY-Algorithm/1-16.js
+++ SY-Algorithm/1-16.js
@@ -1,21 +1,18 @@
-//중복문자제거
+//** 중복문자제거
 //소문자로 된 문자열이 입력되면
 //중복문자 제거
 //문자열의 순서는 유지
 
-//모두 쪼개서 반복문으로 돌리다가
-//중복된게 나오면 빼고 넣어버리자
+
 
 function solution(str){
-    let answer;
-    str.split('').forEach((item,idx) =>{
-        // console.log(item,idx)
-        if(item[idx] !== item[idx+1]){
-            console.log(item[idx]);
-        }
-    })
+    let answer ="";
+    for(let i=0; i<str.length; i++){
+        if(str.indexOf(str[i])===i) answer += str[i];
+    }
+    return answer;
 
 }
 
-solution('hhhhhhhiiiiii');
+console.log(solution('ksekkset'));
 //hi
