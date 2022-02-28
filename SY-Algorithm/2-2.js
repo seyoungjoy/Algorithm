7f8f5fc6aa857597a65c2f2582a854d37d5623c9
seyoungjoy <diddpwl80@gmail.com>
2022-02-28 22:57:15 +0900

    2-2 review

--- SY-Algorithm/2-2.js
+++ SY-Algorithm/2-2.js
@@ -1,16 +1,20 @@
 //보이는 학생
-//
-function solution(arr){         
-    let answer=1, max=arr[0];
-    for(let i=1; i<arr.length; i++){
-        if(arr[i]>max){
-            answer++;
-            max=arr[i];
-        }
-    }
-    return answer;
+//n명의 학생을 일렬로 세움
+//이때 선생님이 맨 앞에서 봤을 때 보이는 사람 숫자를 계산
+//앞에 서 있는 사람보다 커야지 보임
+//앞에 서 있는 사람이랑 같거나 작으면 보이지 않음.
+
+function solution(arr){
+   let answer=1;
+   let max=arr[0];
+   for(let i=1; i<arr.length; i++){
+       if(arr[i]>max){
+           answer ++;
+           max=arr[i];
+       }
+   }
+   console.log(answer)
 }
 
-let arr=[130, 135, 148, 140, 145, 150, 150, 153];
-console.log(solution(arr));
-
+let studentHeightArray = [130,135,148,140,145,150,153];
+solution(studentHeightArray); //5
